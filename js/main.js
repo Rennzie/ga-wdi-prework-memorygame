f3fcc670d926920cf4ba9d39f4acd8a403947f93
Rennzie <Sean.Rennie@randgoldresources.com>
2018-05-18 06:23:11 +0000

    added function logic to game

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
+flipCard =(cardId)=>{
+    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId])
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+    checkForMatch();
+}
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if(cardsInPlay.length === 2){
-    if(cardsInPlay[0] === cardsInPlay[1]){
-        alert('You found a match');
-    }else{
-        alert('Sorry try again');
+checkForMatch = () => {
+    if(cardsInPlay.length === 2){
+        if(cardsInPlay[0] === cardsInPlay[1]){
+            alert('You found a match');
+        }else{
+            alert('Sorry try again');
+        }
     }
 }
+
+flipCard(0);
+flipCard(1);
